6d9a1773ef467f2438eb578fd52f82a9c48a5b5e
Abhay Jirati <abhayjirati@Abhays-MacBook-Air.local>
2021-06-19 16:14:08 +0530

    adding the Event Detail Page

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,7 +1,39 @@
+import { useRouter } from 'next/router';
+
+import { Fragment } from 'react';
+
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
+
+import { getEventById } from '../../dummy-data';
+
 function EventDetailPage () {
-    return <div>
-        <h1>Signle Event</h1>
-    </div>
+    const router = useRouter();
+
+    const eventId = router.query.eventid;
+
+    const event = getEventById(eventId);
+
+    if(!event) {
+        return <p>No Event Fount !</p>
+    }
+
+    return (
+        <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+    )
 }
 
 export default EventDetailPage;
