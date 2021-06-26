0982a2ae7e693cf2f6a86c2fb062233414effadf
Abhay Jirati <abhayjirati@Abhays-MacBook-Air.local>
2021-06-26 21:14:00 +0530

    adding the notification to the sign up user

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,16 @@
 import { Fragment } from "react";
 import MainHeader from "./main-header";
 
+import Notification from '../ui/notification';
+
 function Layout (props) {
     return <Fragment>
+        <Notification>
         <MainHeader />
         <main>
             {props.children}
         </main>
+        </Notification>
     </Fragment>
     
 }
